82332cfd543e89c09dbb15f36a6c31d7036cbeed
sebastianFornillo <sebastianmatiasfornillo@gmail.com>
2024-03-12 21:12:34 -0300

    toDo

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,32 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
+    const formulario = document.forms[0];
+    const email = document.querySelector("#inputEmail");
+    const password = document.querySelector("#inputPassword");
+    const url = 'https://todo-api.ctd.academy/v1';
     
-
-
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function (event) {
-       
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault(); 
+            mostrarSpinner();
         
+        const payload = {
+            email: email.value,
+            password:password.value
+        };
 
-
+        const settings ={
+            method:'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        };
+        realizarLogin(settings);
+        formulario.reset();
 
     });
 
@@ -21,13 +35,32 @@ window.addEventListener('load', function () {
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarLogin(settings) {
-       
+        console.log('lanzamos la consulta a la api');
 
-
-
-
-        
+        fetch(`${url}/users/login`, settings)
+        .then(response =>{
+            console.log(response);
+            if(response.ok != true){
+                alert('algunos de los datos son incorrectos')
+            }
+            return response.json();
+        })
+        .then(data=>{
+            console.log('promesa cumplida');
+            console.log(data);
+            if(data.jwt){
+                localStorage.setItem('jwt',JSON.stringify(data.jwt));
+                ocultarSpinner();
+                location.replace("./mis-tareas.html")
+            
+            }
+        }).catch(err => {
+            console.log("Promesa rechazada:");
+            console.log(err);
+            ocultarSpinner();
+        })
     };
-
-
+    
 });
+
+
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,18 +1,36 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
-
-
-    
+    const formulario = document.forms[0];
+    const nombre = document.querySelector('#inputNombre');
+    const apellido = document.querySelector('#inputApellido');
+    const email = document.querySelector('#inputEmail');
+    const password = document.querySelector('#inputPassword');
+    const url = 'https://todo-api.ctd.academy/v1';
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function (event) {
-      
+    formulario.addEventListener('submit', function (event) {
+      event.preventDefault();  
+      mostrarSpinner();
+    
+    
+    const payload = {
+        firstName: nombre.value,
+        lastName: apellido.value,
+        email: email.value,
+        password: password.value
+    }
+    const settings={
+        method:'POST',
+        body:JSON.stringify(payload),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
 
-
-
+        realizarRegister(settings);
+        formulario.reset();
 
     });
 
@@ -20,12 +38,36 @@ window.addEventListener('load', function () {
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarRegister(settings) {
-        
 
-
-
-
-    };
-
-
+        fetch(`${url}/users`, settings)
+        .then(response =>{
+            if(response.ok != true){
+                alert('logueate de ésta');
+            }
+                return response.json();
+            
+        })
+        .then(data =>{
+                console.log(data);
+                if(data.jwt){
+            
+                    localStorage.setItem('jwt',JSON.stringify(data.jwt));
+                    ocultarSpinner();
+                    location.replace('./mis-tareas.html');
+                }
+            
+        }).catch(err=>{
+            console.log(err);
+            ocultarSpinner();
+        })
+        };
 });
+
+
+
+
+    
+
+
+
+
